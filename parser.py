beeabfb6e64508132bbe2b59cb6a3d7ed65c6c2d
maisab <maaisa.barreto@gmail.com>
2016-05-18 14:12:06 -0300

    semantica

--- parser.py
+++ parser.py
@@ -24,22 +24,22 @@ precedence = (
 #uma funcao
 def p_programa_1(p):
     'programa : principal'
-    p[0] = Tree('programa', [p[1]])
+    p[0] = Tree('programa_principal', [p[1]])
 
 #mais de uma funcao
 def p_programa_2(p):
     'programa : func_loop principal'
-    p[0] = Tree('programa', [p[1],p[2]])
+    p[0] = Tree('programa_funcao', [p[1],p[2]])
 
 #variavel global
 def p_programa_3(p):
     'programa : declara_var programa'
-    p[0] = Tree('programa', [p[1],p[2]])
+    p[0] = Tree('programa_varglobal', [p[1],p[2]])
 
 #funcao principal
 def p_principal(p):
-    'principal : tipo PRINCIPAL ABRE_PAR FECHA_PAR NOVA_LINHA sequencia_decl FIM'
-    p[0] = Tree('principal', [p[1],p[6]])
+    'principal : VAZIO PRINCIPAL ABRE_PAR FECHA_PAR NOVA_LINHA sequencia_decl FIM'
+    p[0] = Tree('principal', [p[6]])
 
 #uma funcao
 def p_func_loop_1(p):
@@ -49,7 +49,7 @@ def p_func_loop_1(p):
 #mais de uma funcao
 def p_func_loop_2(p):
     'func_loop : func_loop func_decl'
-    p[0] = Tree('func_loop', [p[1], p[2]])
+    p[0] = Tree('func_loop_loop', [p[1], p[2]])
 
 #funcao
 def p_func_decl(p):
@@ -64,44 +64,44 @@ def p_sequencia_decl_1(p):
 #mais de uma declaracao
 def p_sequencia_decl_2(p):
     'sequencia_decl : sequencia_decl declaracao'
-    p[0] = Tree('sequencia_decl', [p[1], p[2]])
+    p[0] = Tree('sequencia_decl_loop', [p[1], p[2]])
 
 def p_declaracao_1(p):
     'declaracao : se_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_se', [p[1]])
 
 def p_declaracao_2(p):
     'declaracao : repita_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_repita', [p[1]])
 
 def p_declaracao_3(p):
     'declaracao : atribuicao_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_atribuicao', [p[1]])
 
 def p_declaracao_4(p):
     'declaracao : leia_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_leia', [p[1]])
 
 def p_declaracao_5(p):
     'declaracao : escreva_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_escreva', [p[1]])
 
 def p_declaracao_6(p):
     'declaracao : declara_var'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_declaravar', [p[1]])
 
 #retorna em outra parte do codigo
 def p_declaracao_7(p):
     'declaracao : retorna_decl'
-    p[0] = Tree('declaracao', [p[1]])
+    p[0] = Tree('declaracao_retorna', [p[1]])
 
 def p_retorna_decl_1(p):
     'retorna_decl : RETORNA ABRE_PAR ID FECHA_PAR'
-    p[0] = Tree('retorna', [], [p[3]])
+    p[0] = Tree('retorna_id', [], [p[3]])
 
 def p_retorna_decl_2(p):
     'retorna_decl : RETORNA ABRE_PAR numero_decl FECHA_PAR'
-    p[0] = Tree('retorna', [p[3]])
+    p[0] = Tree('retorna_numero', [p[3]])
 
 def p_numero_1(p):
     'numero_decl : INTEIRO'
@@ -117,7 +117,7 @@ def p_se_decl_1(p):
 
 def p_se_decl_2(p):
     'se_decl : SE exp_decl ENTAO NOVA_LINHA sequencia_decl NOVA_LINHA SENAO NOVA_LINHA sequencia_decl NOVA_LINHA FIM NOVA_LINHA'
-    p[0] = Tree('se_decl', [p[2], p[5], p[9]])
+    p[0] = Tree('se_decl_senao', [p[2], p[5], p[9]])
 
 def p_repita_decl(p):
     'repita_decl : REPITA NOVA_LINHA sequencia_decl ATE exp_decl NOVA_LINHA'
@@ -133,7 +133,7 @@ def p_leia_decl(p):
 
 def p_escreva_decl_1(p):
     'escreva_decl : ESCREVA ABRE_PAR exp_decl FECHA_PAR NOVA_LINHA'
-    p[0] = Tree('escreva_decl', [p[3]])
+    p[0] = Tree('escreva_decl_exp', [p[3]])
 
 def p_escreva_decl_2(p):
     'escreva_decl : ESCREVA ABRE_PAR chamada_func FECHA_PAR NOVA_LINHA'
@@ -149,11 +149,11 @@ def p_parametro_chama_func_1(p):
 
 def p_parametro_chama_func_2(p):
     'parametro_chama_func : numero_decl'
-    p[0] = Tree('parametro_chama_func', [p[1]])
+    p[0] = Tree('parametro_chama_func_num', [p[1]])
 
 def p_parametro_chama_func_3(p):
     'parametro_chama_func : parametro_chama_func ID'
-    p[0] = Tree('parametro_chama_func', [p[1]], [p[2]])
+    p[0] = Tree('parametro_chama_func_paramentro', [p[1]], [p[2]])
 
 def p_exp_decl_1(p):
     'exp_decl : simples_exp'
@@ -161,7 +161,7 @@ def p_exp_decl_1(p):
 
 def p_exp_decl_2(p):
     'exp_decl : simples_exp compara_op simples_exp'
-    p[0] = Tree('exp_decl', [p[1], p[2], p[3]])
+    p[0] = Tree('exp_decl_compara', [p[1], p[2], p[3]])
 
 def p_compara_op_1(p):
     'compara_op : IGUAL'
@@ -189,7 +189,7 @@ def p_simples_exp_1(p):
 
 def p_simples_exp_2(p):
     'simples_exp : simples_exp soma_sub termo'
-    p[0] = Tree('simples_exp', [p[1], p[2], p[3]])
+    p[0] = Tree('simples_exp_somasub', [p[1], p[2], p[3]])
 
 def p_soma_sub_1(p):
     'soma_sub : SOMA'
@@ -201,11 +201,11 @@ def p_soma_sub_2(p):
 
 def p_termo_1(p):
     'termo : fator'
-    p[0] = Tree('termo', [p[1]])
+    p[0] = Tree('termo_fator', [p[1]])
 
 def p_termo_2(p):
     'termo : termo mult_div fator'
-    p[0] = Tree('termo', [p[1], p[2], p[3]])
+    p[0] = Tree('termo_multdiv', [p[1], p[2], p[3]])
 
 def p_mult_div_1(p):
     'mult_div : MULT'
@@ -217,23 +217,15 @@ def p_mult_div_2(p):
 
 def p_fator_1(p):
     'fator : ID'
-    p[0] = Tree('fator', [], [p[1]])
-
-# def p_fator_2(p):
-#     'fator : INTEIRO'
-#     p[0] = Tree('fator', [], [p[1]])
-
-# def p_fator_3(p):
-#     'fator : FLUTUANTE'
-#     p[0] = Tree('fator', [], [p[1]])
+    p[0] = Tree('fator_id', [], [p[1]])
 
 def p_fator_2(p):
     'fator : numero_decl'
-    p[0] = Tree('fator', [p[1]])
+    p[0] = Tree('fator_numero', [p[1]])
 
 def p_fator_3(p):
     'fator : ABRE_PAR exp_decl FECHA_PAR'
-    p[0] = Tree('fator', [p[2]])
+    p[0] = Tree('fator_exp', [p[2]])
 
 def p_declara_var(p):
     'declara_var : tipo DOIS_PONTOS ID NOVA_LINHA'
@@ -241,7 +233,7 @@ def p_declara_var(p):
 
 def p_parametro_decl_1(p):
     'parametro_decl : parametro_decl  VIRGULA tipo DOIS_PONTOS ID'
-    p[0] = Tree('parametro_decl', [p[1], p[3]], [p[5]])
+    p[0] = Tree('parametro_decl_loop', [p[1], p[3]], [p[5]])
 
 def p_parametro_decl_2(p):
     'parametro_decl : tipo DOIS_PONTOS ID'
@@ -262,13 +254,13 @@ def p_tipo_3(p):
 def p_error(p):
     if p:
         print("Erro sintático: '%s', linha %d" % (p.value, p.lineno))
-        #exit(1)
-        p.lexer.skip(1)
+        exit(1)
+        #p.lexer.skip(1)
     else:
         yacc.restart()
         print('Erro sintático: definições incompletas!')
-        #exit(1)
-        p.lexer.skip(1)
+        exit(1)
+        #p.lexer.skip(1)
 
 def parse_tree(code):
     parser = yacc.yacc(debug=True)
---
+++ semantica.py
@@ -0,0 +1,398 @@
+from parser import *
+import collections
+
+#Contar quantos parametros passa na função
+
+class Semantica():
+
+    def __init__(self, codigo):
+        self.table = {}
+        self.scope = "global"
+        self.tree = parser. parse_tree(codigo)
+
+    def raiz(self):
+        if(self.tree.type == "programa_principal"):
+            self.scope = "principal"
+            self.principal[self.tree.child[0]]
+            self.scope = "global"
+
+        if(self.tree.type == "programa_funcao"):
+            self.func_loop(self.tree.child[0])
+            self.principal(self.tree.child[1])
+
+        if(self.tree.type == "programa_varglobal"):
+            self.programa(self.tree.child[1])
+            self.declara_var(self.tree.child[0])
+
+
+# def p_principal(p):
+#     'principal : VAZIO PRINCIPAL ABRE_PAR FECHA_PAR NOVA_LINHA sequencia_decl FIM'
+#     p[0] = Tree('principal', [p[6]])
+
+    def principal(self, node):
+        self.sequencia_decl(node.child[0])
+
+# def p_func_loop_1(p):
+#     'func_loop : func_decl'
+#     p[0] = Tree('func_loop', [p[1]])
+
+# def p_func_loop_2(p):
+#     'func_loop : func_loop func_decl'
+#     p[0] = Tree('func_loop_loop', [p[1], p[2]])
+
+    def func_loop(self, node):
+        #if(node.child == func_decl)
+        if(len(node.child) == 2) :
+            self.func_loop(node.child[0])
+            self.func_decl(node.child[1])
+        else:
+            self.func_decl(node.child[0])
+
+# def p_func_decl(p):
+#     'func_decl : tipo ID ABRE_PAR parametro_decl FECHA_PAR NOVA_LINHA sequencia_decl FIM NOVA_LINHA'
+#     p[0] = Tree('func_decl', [p[1], p[4], p[7]], [p[2]])
+
+    def func_decl(self, node):
+        if(node.value[0] is self.table.keys()) : #se ja tem funcao com esse nome na tabela
+            print("Erro Semântico, o nome "+  node.value[0] + " já esta sendo utilizado")
+            exit(1)
+
+        self.scope = node.value[0]  #escopo nome da função
+        self.parametro_decl(node.child[1])
+        self.sequencia_decl(node.child[3])
+
+        self.table[node.value[0]] = {}
+        self.table[node.value[0]]["var"] = false
+        self.table[node.value[0]]["tipo"] = self.tipo(node.child[0])
+
+        self.scope = "global"
+
+# def p_sequencia_decl_1(p):
+#     'sequencia_decl : declaracao'
+#     p[0] = Tree('sequencia_decl', [p[1]])
+
+# def p_sequencia_decl_2(p):
+#     'sequencia_decl : sequencia_decl declaracao'
+#     p[0] = Tree('sequencia_decl_loop', [p[1], p[2]])
+
+    def sequencia_decl(self, node):
+        if(len(node.child) == 2) :
+            self.sequencia_decl(node.child[0])
+            self. declarao(node.child[1])
+        else :
+            self. declarao(node.child[0])
+
+    def declaracao(self, node) :
+        if(node.child[0].type == "declaracao_se") :
+            self.se_decl(node.child[0])
+
+        elif(node.child[0].type == "declaracao_repita") :
+            self.repita_decl(node.child[0])
+
+        elif(node.child[0].type == "declaracao_atribuicao") :
+            self.atribuicao_decl(node.child[0])
+
+        elif(node.child[0].type == "declaracao_leia") :
+            self.leia_decl(node.child[0])
+
+        elif(node.child[0].type == "declaracao_escreva") :
+            self.escreva_decl(node.child[0])
+
+        elif(node.child[0].type == "declaracao_declaravar") :
+            self.declara_var(node.child[0])
+
+        elif(node.child[0].type == "declaracao_retorna") :
+            self.retorna_decl(node.child[0])
+
+
+# def p_retorna_decl_1(p):
+#     'retorna_decl : RETORNA ABRE_PAR ID FECHA_PAR'
+#     p[0] = Tree('retorna_id', [], [p[3]])
+
+# def p_retorna_decl_2(p):
+#     'retorna_decl : RETORNA ABRE_PAR numero_decl FECHA_PAR'
+#     p[0] = Tree('retorna_numero', [p[3]])
+
+    def retorna_declaracao(self, node) :
+        if (node.child[0].type == "retorna_numero") :
+            self.numero_decl(node.child[0])
+        else :
+            return node.value[0] #retorna o id
+
+# def p_numero_1(p):
+#     'numero_decl : INTEIRO'
+#     p[0] = Tree('numero_decl_inteiro', [], [p[1]])
+
+# def p_numero_2(p):
+#     'numero_decl : FLUTUANTE'
+#     p[0] = Tree('numero_decl_flutuante', [], [p[1]])
+
+    def numero_decl(self, node) :
+        return node.value[0]
+
+# def p_se_decl_1(p):
+#     'se_decl : SE exp_decl ENTAO NOVA_LINHA sequencia_decl NOVA_LINHA FIM NOVA_LINHA'
+#     p[0] = Tree('se_decl', [p[2], p[5]])
+
+# def p_se_decl_2(p):
+#     'se_decl : SE exp_decl ENTAO NOVA_LINHA sequencia_decl NOVA_LINHA SENAO NOVA_LINHA sequencia_decl NOVA_LINHA FIM NOVA_LINHA'
+#     p[0] = Tree('se_decl_senao', [p[2], p[5], p[9]])
+
+    def se_decl(self, node) :
+        if(len(node.child) == 2) :
+            self.exp_decl(node.child[0])
+            self.sequencia_decl(node.child[1])
+
+        else :
+            self.exp_decl(node.child[0])
+            self.sequencia_decl(node.child[1])
+            self.sequencia_decl(node.child[2])
+
+# def p_repita_decl(p):
+#     'repita_decl : REPITA NOVA_LINHA sequencia_decl ATE exp_decl NOVA_LINHA'
+#     p[0] = Tree('repita_decl', [p[3], p[5]])
+
+    def repita_decl(self, node) :
+        self.sequencia_decl(node.child[0])
+        self.exp_decl(node.child[1])
+
+# def p_atribuicao_decl(p):
+#     'atribuicao_decl : ID ATRIBUICAO exp_decl NOVA_LINHA'
+#     p[0] = Tree('atribuicao_decl', [p[3]], [p[1]])
+
+    def atribuicao_decl(self, node) :
+        self.exp
+
+# def p_leia_decl(p):
+#     'leia_decl : LEIA ABRE_PAR ID FECHA_PAR NOVA_LINHA'
+#     p[0] = Tree('leia_decl', [], [p[3]])
+
+    def leia_decl(self, node) :
+        if (self.scope + "." + node.value[0] not in self.table.keys()) :
+            print("Erro Semântico. Variável " + node.value[0] + " não encontrada")
+            exit(1)
+
+
+# def p_escreva_decl_1(p):
+#     'escreva_decl : ESCREVA ABRE_PAR exp_decl FECHA_PAR NOVA_LINHA'
+#     p[0] = Tree('escreva_decl_exp', [p[3]])
+
+# def p_escreva_decl_2(p):
+#     'escreva_decl : ESCREVA ABRE_PAR chamada_func FECHA_PAR NOVA_LINHA'
+#     p[0] = Tree('escreva_decl', [p[3]])
+
+    def escreva_decl(self, node) :
+        if(node.child[0].type == "escreva_decl_exp") :
+            self.exp_decl(node.child[0])
+
+        else :
+            self.chamada_func(node.child[0])
+
+
+# def p_chamada_func(p):
+#     'chamada_func : ID ABRE_PAR parametro_chama_func FECHA_PAR'
+#     p[0] = Tree('chamada_func', [p[3]], [p[1]])
+
+    def chamada_func(self, node) :
+
+        if(node.value[0] not in self.table.keys()) :
+            print("Erro Semântico, nome de funcao não declarado : " + node.value[0] )
+            exit(1)
+
+        self.parametro_chama_func(node.child[0])
+
+# def p_parametro_chama_func_1(p):
+#     'parametro_chama_func : ID'
+#     p[0] = Tree('parametro_chama_func', [], [p[1]])
+
+# def p_parametro_chama_func_2(p):
+#     'parametro_chama_func : numero_decl'
+#     p[0] = Tree('parametro_chama_func_num', [p[1]])
+
+# def p_parametro_chama_func_3(p):
+#     'parametro_chama_func : parametro_chama_func ID'
+#     p[0] = Tree('parametro_chama_func_paramentro', [p[1]], [p[2]])
+
+    def parametro_chama_func(self, node) :
+        if ( node.child[0].type == "parametro_chama_func_num") :
+            self.numero_decl(node.child[0])
+
+        else (node.child[0].type == "parametro_chama_func")
+            self.parametro_chama_func(node.child[0])
+
+
+# def p_exp_decl_1(p):
+#     'exp_decl : simples_exp'
+#     p[0] = Tree('exp_decl', [p[1]])
+
+# def p_exp_decl_2(p):
+#     'exp_decl : simples_exp compara_op simples_exp'
+#     p[0] = Tree('exp_decl_compara', [p[1], p[2], p[3]])
+
+    def exp_decl(self, node) :
+        if( node.child[0].type == "exp_decl_compara" ) :
+            self.simples_exp(node.child[0])
+            self.compara_op(node.child[1])
+            self.compara_op(node.child[2])
+
+        else :
+            self.simples_exp(node.child[0])
+
+# def p_compara_op_1(p):
+#     'compara_op : IGUAL'
+#     p[0] = Tree('compara_op_igual', [], [p[1]])
+
+# def p_compara_op_2(p):
+#     'compara_op : MENOR'
+#     p[0] = Tree('compara_op_menor', [], [p[1]])
+
+# def p_compara_op_3(p):
+#     'compara_op : MAIOR'
+#     p[0] = Tree('compara_op_maior', [], [p[1]])
+
+# def p_compara_op_4(p):
+#     'compara_op : MENOR_IGUAL'
+#     p[0] = Tree('compara_op_menorIgual', [], [p[1]])
+
+# def p_compara_op_5(p):
+#     'compara_op : MAIOR_IGUAL'
+#     p[0] = Tree('compara_op_maiorIgual', [], [p[1]])
+
+    def compara_op(self, node) :
+            return node.value[0]
+
+# def p_simples_exp_1(p):
+#     'simples_exp : termo'
+#     p[0] = Tree('simples_exp', [p[1]])
+
+# def p_simples_exp_2(p):
+#     'simples_exp : simples_exp soma_sub termo'
+#     p[0] = Tree('simples_exp_somasub', [p[1], p[2], p[3]])
+
+    def simples_exp(self, node) :
+        if(node.child[0].type == "simples_exp_somasub") :
+            self.simples_exp(node.child[0])
+            self.soma_sub(node.child[1])
+            self.termo(node.child[2])
+
+        else :
+            self.termo(node.child[0])
+
+# def p_soma_sub_1(p):
+#     'soma_sub : SOMA'
+#     p[0] = Tree('soma_sub_soma', [], [p[1]])
+
+# def p_soma_sub_2(p):
+#     'soma_sub : SUB'
+#     p[0] = Tree('soma_sub_subtracao', [], [p[1]])
+
+    def soma_sub(self, node) :
+        return node.child[0]
+
+# def p_termo_1(p):
+#     'termo : fator'
+#     p[0] = Tree('termo_fator', [p[1]])
+
+# def p_termo_2(p):
+#     'termo : termo mult_div fator'
+#     p[0] = Tree('termo_multdiv', [p[1], p[2], p[3]])
+
+    def termo(self, node) :
+        if(node.child[0].type == "termo_multdiv") :
+            self.termo(node.child[0])
+            self.mult_div(node.child[1])
+            self.fator(node.child[2])
+
+        else :
+            self.fator(node.child[0])
+
+# def p_mult_div_1(p):
+#     'mult_div : MULT'
+#     p[0] = Tree('mult_div_multiplicacao', [], [p[1]])
+
+# def p_mult_div_2(p):
+#     'mult_div : DIVISAO'
+#     p[0] = Tree('mult_div_divisao', [], [p[1]])
+
+    def mult_div(self, node) :
+            return node.value[0]
+
+# def p_fator_1(p):
+#     'fator : ID'
+#     p[0] = Tree('fator_id', [], [p[1]])
+
+# def p_fator_2(p):
+#     'fator : numero_decl'
+#     p[0] = Tree('fator_numero', [p[1]])
+
+# def p_fator_3(p):
+#     'fator : ABRE_PAR exp_decl FECHA_PAR'
+#     p[0] = Tree('fator_exp', [p[2]])
+
+    def fator(self, node) :
+        if(node.child[0].type == "fator_numero") :
+            self.numero_decl(node.child[0])
+
+        elif  (node.child[0].type == "fator_exp") :
+            self.exp_decl(node.child[0])
+
+        else :
+            #verificar se o fator esta inicializado
+
+
+# def p_tipo_1(p):
+#     'tipo : VAZIO'
+#     p[0] = Tree('tipo_vazio', [], [p[1]])
+
+# def p_tipo_2(p):
+#     'tipo : INTEIRO'
+#     p[0] = Tree('tipo_inteiro', [], [p[1]])
+
+# def p_tipo_3(p):
+#     'tipo : FLUTUANTE'
+#     p[0] = Tree('tipo_flutuante', [], [p[1]])
+
+    def tipo(self, node) :
+        return node.value[0]
+
+# def p_declara_var(p):
+#     'declara_var : tipo DOIS_PONTOS ID NOVA_LINHA'
+#     p[0] = Tree('declara_var', [p[1]], [p[3]])
+
+    def declara_var(self, node) :
+        if(self.scope + "." + node.value[0] is self.table.keys()): #se ja tem variavel com esse nome na tabela
+            print("Erro Semântico, nome já utilizado : " + node.value[0] )
+            exit(1)
+
+        self.table[self.scope + "." + node.value[0]] = {}
+        self.table[self.scope + "." + node.value[0]]["var"] = true
+        self.table[self.scope + "." + node.value[0]]["tipo"] = self.tipo(node.child[0])
+
+# def p_parametro_decl_1(p):
+#     'parametro_decl : parametro_decl  VIRGULA tipo DOIS_PONTOS ID'
+#     p[0] = Tree('parametro_decl_loop', [p[1], p[3]], [p[5]])
+
+# def p_parametro_decl_2(p):
+#     'parametro_decl : tipo DOIS_PONTOS ID'
+#     p[0] = Tree('parametro_decl', [p[1]], [p[3]])
+
+    def parametro_decl(self, node) :
+        if(node.chil
+            d[0].type == "parametro_decl_loop") :
+            self.parametro_decl(node.child[0])
+
+            self.table[self.scope + "." + node.value[0]] = {}
+            self.table[self.scope + "." + node.value[0]]["var"] = true
+            self.table[self.scope + "." + node.value[0]]["tipo"] = self.tipo(node.child[1])
+
+        else :
+            self.table[self.scope + "." + node.value[0]] = {}
+            self.table[self.scope + "." + node.value[0]]["var"] = true
+            self.table[self.scope + "." + node.value[0]]["tipo"] = self.tipo(node.child[0])
+
+if __name__ == '__main__':
+    import sys
+    code = open(sys.argv[1])
+    s = Semantica(code.read())
+    s.semanticaTopo()
+    print("Tabela de Simbolos:", s.simbolos)
